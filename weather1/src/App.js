fdc545aa5c2db30ffc75ff6bfda9487977d2df14
Stinger15524 <149871038+Stinger15524@users.noreply.github.com>
2024-05-30 11:49:06 +0600

    настроена карта и локализация

--- weather1/src/App.js
+++ weather1/src/App.js
@@ -3,22 +3,47 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootswatch/dist/journal/bootstrap.css";
 import { Navbar, Nav, Row, Col, Container, Form, Button } from "react-bootstrap";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 const locales = {
     ru: {
-        weather: 'Погода',cd 
+        weather: 'Погода',
         searchPlaceholder: 'Поиск города',
         searchButton: 'Поиск',
+        temperature: 'Температура',
+        feelsLike: 'Ощущается как',
+        high: 'Максимум',
+        low: 'Минимум',
+        humidity: 'Влажность',
+        chanceOfPrecipitation: 'Вероятность осадков',
+        windSpeed: 'Скорость ветра',
+        pressure: 'Давление',
+        uvIndex: 'УФ-индекс',
+        dewPoint: 'Точка росы',
+        visibility: 'Видимость',
+        in: 'в',
         ru: 'RU',
-        en: 'EN',
+        en: 'EN'
     },
     en: {
         weather: 'Weather',
         searchPlaceholder: 'Search city',
         searchButton: 'Search',
+        temperature: 'Temperature',
+        feelsLike: 'Feels Like',
+        high: 'High',
+        low: 'Low',
+        humidity: 'Humidity',
+        chanceOfPrecipitation: 'Chance of Precipitation',
+        windSpeed: 'Wind Speed',
+        pressure: 'Pressure',
+        uvIndex: 'UV Index',
+        dewPoint: 'Dew Point',
+        visibility: 'Visibility',
+        in: 'in',
         ru: 'RU',
-        en: 'EN',
+        en: 'EN'
     }
 };
 
@@ -78,10 +103,10 @@ const App = () => {
                             {locales[locale].searchButton}
                         </Button>
                         {searchResult && (
-                            <WeatherDisplay weatherData={searchResult} />
+                            <WeatherDisplay weatherData={searchResult} locale={locale} />
                         )}
                     </Col>
-                    <Col md={6}>
+                    <Col md={6} style={{ height: '500px' }}>
                         {searchResult && (
                             <WeatherMap weatherData={searchResult} />
                         )}
@@ -92,29 +117,49 @@ const App = () => {
     );
 };
 
-const WeatherDisplay = ({ weatherData }) => {
-    const weather = weatherData.weather[0];
-    const iconUrl = `http://openweathermap.org/img/w/${weather.icon}.png`;
+const WeatherDisplay = ({ weatherData, locale }) => {
+    const weather = weatherData.weather ? weatherData.weather[0] : {};
+    const iconUrl = weather.icon ? `http://openweathermap.org/img/w/${weather.icon}.png` : '';
 
     return (
         <div>
             <h1>
-                {weather.main} in {weatherData.name}
-                <img src={iconUrl} alt={weather.description} />
+                {weather.main} {locales[locale].in} {weatherData.name}
+                {iconUrl && <img src={iconUrl} alt={weather.description} />}
             </h1>
-            <p>Temperature: {weatherData.main.temp}°C</p>
-            <p>Feels Like: {weatherData.main.feels_like}°C</p>
-            <p>High: {weatherData.main.temp_max}°C</p>
-            <p>Low: {weatherData.main.temp_min}°C</p>
-            <p>Humidity: {weatherData.main.humidity}%</p>
-            <p>Chance of Precipitation: {weatherData.clouds.all}%</p>
+            <p>{locales[locale].temperature}: {weatherData.main?.temp}°C</p>
+            <p>{locales[locale].feelsLike}: {weatherData.main?.feels_like}°C</p>
+            <p>{locales[locale].high}: {weatherData.main?.temp_max}°C</p>
+            <p>{locales[locale].low}: {weatherData.main?.temp_min}°C</p>
+            <p>{locales[locale].humidity}: {weatherData.main?.humidity}%</p>
+            <p>{locales[locale].chanceOfPrecipitation}: {weatherData.clouds?.all}%</p>
+            <p>{locales[locale].windSpeed}: {weatherData.wind?.speed} m/s</p>
+            <p>{locales[locale].pressure}: {weatherData.main?.pressure} hPa</p>
+            <p>{locales[locale].uvIndex}: {weatherData.uvi || 'N/A'}</p>
+            <p>{locales[locale].dewPoint}: {weatherData.main?.dew_point || 'N/A'}°C</p>
+            <p>{locales[locale].visibility}: {weatherData.visibility ? weatherData.visibility / 1000 : 'N/A'} km</p>
         </div>
     );
 };
 
 const WeatherMap = ({ weatherData }) => {
+    const MapEffect = ({ coords }) => {
+        const map = useMap();
+        useEffect(() => {
+            map.setView(coords, 10);
+        }, [coords, map]);
+
+        return null;
+    };
+
+    if (!weatherData.coord) return null;
+
     return (
-        <MapContainer center={[weatherData.coord.lat, weatherData.coord.lon]} zoom={10} style={{ height: '400px', width: '100%' }}>
+        <MapContainer
+            center={[weatherData.coord.lat, weatherData.coord.lon]}
+            zoom={10}
+            style={{ height: '100%', width: '100%' }}
+        >
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
@@ -123,6 +168,7 @@ const WeatherMap = ({ weatherData }) => {
                     {weatherData.name}
                 </Popup>
             </Marker>
+            <MapEffect coords={[weatherData.coord.lat, weatherData.coord.lon]} />
         </MapContainer>
     );
 };
